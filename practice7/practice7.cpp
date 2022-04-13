f61cd48adc14c223a17ccaabae61824bd87d4932
ohne <ghdlr785@gmail.com>
2022-04-13 15:29:43 +0900

    다이나믹 캐스

--- practice7/practice7.cpp
+++ practice7/practice7.cpp
@@ -5,6 +5,17 @@ int main()
 {
     Character* pHero = new Warrior;
     pHero->Attack();
-    ((Warrior*)pHero)->DoubleSlash();
+
+    //((Warrior*)pHero)->DoubleSlash();
+    Warrior* pWarrior = dynamic_cast<Warrior*>(pHero);
+    if (pWarrior == nullptr)
+    {
+        std::cout << "Error" << std::endl;
+    }
+    else
+    {
+        pWarrior->DoubleSlash();
+    }
+
     delete pHero;
 }
